7dfc0665fe5d7ee8f5c4843092d6a2eda8b5f583
ZeroxTV <leonbaronick@gmail.com>
2016-06-10 10:03:18 +0200

    JFrame added

--- ZTools/src/de/ZeroxTV/ZTools/Main.java
+++ ZTools/src/de/ZeroxTV/ZTools/Main.java
@@ -1,5 +1,23 @@
 package de.ZeroxTV.ZTools;
 
-public class Main {
+import javax.swing.JFrame;
+import javax.swing.JScrollPane;
+import javax.swing.JTextField;
+import javax.swing.JTextPane;
+import javax.swing.text.StyledDocument;
 
+public class Main {
+	
+	public static void main(String[] args) {
+		new Main();
+	}
+	
+	public JFrame frame;
+	public JTextPane console;
+	public JTextField input;
+	public JScrollPane scrollpane;
+	
+	public StyledDocument document;
+	
+	boolean trace = false;
 }
